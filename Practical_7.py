f8f29d6dbc5b1928ff6c0ee7d2126a55f38cebf3
Dhruvish09 <pateldhruvish612000@gmail.com>
2021-07-02 14:13:56 +0530

    update

---
+++ Practical_7.py
@@ -0,0 +1,22 @@
+# 7). Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+# Suppose the following input is supplied to the prografor i in range(1,7):
+#     for j in range(1,7):
+#         if j<=i:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")	
+
+#     print()m:
+# 9
+# Then, the output should be:
+# 11106
+
+value = input("Enter value: ")
+
+n1 = value * 1
+n2 = value * 2
+n3 = value * 3
+n4 = value * 4
+
+total = int(n1) + int(n2) + int(n3) + int(n4)
+print(total)
---
+++ Practical_8.py
@@ -0,0 +1,9 @@
+# 8). Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following:
+# Suppose the following input is supplied to the program:
+# D 300
+# D 300
+# W 200
+# D 100
+# D means deposit while W means withdrawal.
+# Then, the output should be:
+# 500
