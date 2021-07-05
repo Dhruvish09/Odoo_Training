2abf97c83c8df6268c8d4bff462811d15c9dde4a
Dhruvish09 <pateldhruvish612000@gmail.com>
2021-07-05 12:03:30 +0530

    update

--- Practical_32.py
+++ Practical_32.py
@@ -1,9 +1,11 @@
-n = int(input("Enter the number of rows: "))  
-m = (2 * n) - 2  
-for i in range(0, n):  
-    for j in range(0, m):  
+# 32). Write a program to generate Triangle pyramid Pattern:
+
+Number= int(input("Enter the number of rows: "))  
+k = (2 * Number) - 2  
+for i in range(0, Number):  
+    for j in range(0, k):  
         print(end=" ")  
-    m = m - 1  # decrementing m after each loop  
+    k = k - 1  # decrementing m after each loop  
     for j in range(0, i + 1):  
         # printing full Triangle pyramid using stars  
         print("* ", end=' ')  
--- Practical_33.py
+++ Practical_33.py
@@ -1,17 +1,21 @@
-Num1 = int(input("Enter Your Wise Number:"))
-Num2 = int(input("Enter Your Wise Number:"))
+# 33). Write a program to generate Triangle Inverse pyramid Pattern:
 
-for i in range(Num1,Num2):
-    print(" "*((Num2-1)-i)," *"*(i))
 
-# rows = int(input("Enter the number of rows: "))
-# k = rows - 2
-# # This is used to print the downward pyramid
-# for i in range(rows, -1, -1):
-#     for j in range(k, 0, -1):
-#         print(end=" ")
-#     k = k + 1
-#     for j in range(0, i+1):
-#         print("* ", end="")
-#     print()
+# Num1 = int(input("Enter Your Wise Number:"))
+# Num2 = int(input("Enter Your Wise Number:"))
+
+# for i in range(Num1,Num2):
+#     print(" "*((Num2-1)-i)," *"*(i))
+
+Number = int(input("Enter the number: "))
+k = Number - 2 #  number of spaces  
+
+
+for i in range(Number, -1, -1):
+    for j in range(k, 0, -1):
+        print(end=" ")
+    k = k + 1 # Incerement k value after each iteration 
+    for j in range(0, i+1):
+        print("* ", end="")
+    print()
 
