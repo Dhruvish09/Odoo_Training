409a6abc582bd0a21346d929c024e1c277d11f45
Dhruvish09 <pateldhruvish612000@gmail.com>
2021-07-05 11:37:12 +0530

    Update Practical_32.py

--- Practical_32.py
+++ Practical_32.py
@@ -1,9 +1,10 @@
-rows = int(input("Enter the number of rows:"))  
-k = 2 * rows - 2  # It is used for number of spaces  
-for i in range(0, rows):  
-    for j in range(0, k):  
+n = int(input("Enter the number of rows: "))  
+m = (2 * n) - 2  
+for i in range(0, n):  
+    for j in range(0, m):  
         print(end=" ")  
-    k = k - 2   # decrement k value after each iteration  
+    m = m - 1  # decrementing m after each loop  
     for j in range(0, i + 1):  
-        print("* ", end="")  # printing star  
-    print("")  
+        # printing full Triangle pyramid using stars  
+        print("* ", end=' ')  
+    print(" ")  
